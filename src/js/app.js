ca8db187f926cab7c30cf95752113c1648af38f1
Nikita0708 <91466168+Nikita0708@users.noreply.github.com>
2023-07-06 21:44:31 +0200

    add scrollTrigger

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,83 @@
-gsap.registerPlugin(ScrollSmoother)
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
 
-ScrollSmoother.create({
+if (ScrollTrigger.isTouch !== 1) {
+    ScrollSmoother.create({
     wrapper: '.wrapper',
     content: '.content',
     smooth: 1.5,
     effects: true,
-}) 
+    }) 
+    gsap.fromTo('.header-hero', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.header-hero',
+            start: 'center',
+            end: 900,
+            scrub: true
+        }
+    })
+    gsap.fromTo('.achievments', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.achievments',
+            start: 'center',
+            end: 'bottom',
+            scrub: true
+        }
+    })
+    gsap.fromTo('.cards-slider', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.cards-slider',
+            start: 'center',
+            end: 'bottom',
+            scrub: true
+        }
+    })
+    gsap.fromTo('.description', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.description',
+            start: 'center',
+            end: 'bottom',
+            scrub: true
+        }
+    })
+    gsap.fromTo('.services', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.services',
+            start: 'center',
+            end: 'bottom',
+            scrub: true
+        }
+    })
+    gsap.fromTo('.preferences', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.preferences',
+            start: 'center',
+            end: 'bottom',
+            scrub: true
+        }
+    })
+    gsap.fromTo('.works', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.works',
+            start: 'center',
+            end: 'bottom',
+            scrub: true
+        }
+    })
+    gsap.fromTo('.section-form', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.section-form',
+            start: 'center',
+            end: 'bottom',
+            scrub: true
+        }
+    })
+}
+
